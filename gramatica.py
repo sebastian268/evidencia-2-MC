e7d253614bd1aa5ff71bea5e5d6af3541373853b
sebastian268 <A01278278@tec.mx>
2025-05-01 00:40:08 -0600

    Update gramatica.py

--- gramatica.py
+++ gramatica.py
@@ -3,35 +3,75 @@ from nltk import CFG
 nltk.download('punkt')
 
 grammar = CFG.fromstring("""
-oracion -> sujeto verbo oracion_p
-oracion_p -> objeto
-oracion_p -> 
+oración → sujeto verbo oración'
+oración' → objeto
+oración' → 
 
-sujeto -> frase_nominal
-objeto -> frase_nominal
+sujeto → frase_nominal
 
-frase_nominal -> frase_nominal_inicial frase_nominal_p
+objeto → frase_nominal
 
-frase_nominal_inicial -> determinante sustantivo adjetivo
-frase_nominal_inicial -> sustantivo frase_nominal_s_p
-frase_nominal_inicial -> pronombre
+frase_nominal → frase_nominal_inicial frase_nominal'
 
-frase_nominal_s_p -> adjetivo
-frase_nominal_s_p -> 
+frase_nominal_inicial → determinante sustantivo adjetivo
+frase_nominal_inicial → sustantivo frase_nominal_s'
+frase_nominal_inicial → pronombre
 
-frase_nominal_p -> conjuncion frase_nominal
-frase_nominal_p -> 
+frase_nominal_s' → adjetivo
+frase_nominal_s' → 
 
-verbo -> raiz_verbal sufijo_verbal
+frase_nominal' → conjunción frase_nominal
+frase_nominal' → 
 
-raiz_verbal -> "jagon" | "vāedar" | "kēliar" | "rhaenagon" | "drējelagon"
-sufijo_verbal -> "i" | "is" | "ir" | "ion"
+verbo → raíz_verbal sufijo_verbal
 
-determinante -> "ābra" | "ziry" | "bisy" | "morys" | "velg" | "līr" | "tīr"
-sustantivo -> "valzȳrys" | "zaldrīzes" | "azantys" | "qintir" | "voktys" | "melos" | "dūri" | "rhaegon"
-adjetivo -> "sȳz" | "hāedar" | "zȳhon" | "velos" | "rhaeshisar" | "zalar" | "sūngar"
-pronombre -> "nyke" | "ao" | "zȳhon" | "ses" | "til" | "ōs"
-conjuncion -> "vose" | "lōr" | "se"
+raíz_verbal → "jagon"
+raíz_verbal → "vāedar"
+raíz_verbal → "kēliar"
+raíz_verbal → "rhaenagon"
+raíz_verbal → "drējelagon"
+
+sufijo_verbal → "i"
+sufijo_verbal → "is"
+sufijo_verbal → "ir"
+sufijo_verbal → "ion"
+
+determinante → "ābra"
+determinante → "ziry"
+determinante → "bisy"
+determinante → "morys"
+determinante → "velg"
+determinante → "līr"
+determinante → "tīr"
+
+sustantivo → "valzȳrys"
+sustantivo → "zaldrīzes"
+sustantivo → "azantys"
+sustantivo → "qintir"
+sustantivo → "voktys"
+sustantivo → "melos"
+sustantivo → "dūri"
+sustantivo → "rhaegon"
+
+adjetivo → "sȳz"
+adjetivo → "hāedar"
+adjetivo → "zȳhon"
+adjetivo → "velos"
+adjetivo → "rhaeshisar"
+adjetivo → "zalar"
+adjetivo → "sūngar"
+
+pronombre → "nyke"
+pronombre → "ao"
+pronombre → "zȳhon"
+pronombre → "ses"
+pronombre → "til"
+pronombre → "ōs"
+
+conjunción → "vose"
+conjunción → "lōr"
+conjunción → "se"
+
 """)
 
 parser = nltk.ChartParser(grammar)
